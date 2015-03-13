850dc7970538f2137e1e78c538c760c8ee2a3d0d
domurtag <5ubmarin3>
2015-03-13 21:10:44 +0000

    initial commit

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,33 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolver = "maven" // or ivy
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        // runtime 'mysql:mysql-connector-java:5.1.27'
+    }
+
+    plugins {
+        build(":release:3.0.1", ":rest-client-builder:1.0.3") {
+            export = false
+        }
+    }
+}
